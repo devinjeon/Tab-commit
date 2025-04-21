62685178d6c0f348137eecc9e5b91f716a1ec2d5
Hyojun Jeon <gywns2702@gmail.com>
2025-04-21 11:14:11 +0900

    feat: remove autosuggest strategy function

--- tabmit-autocomplete.zsh
+++ tabmit-autocomplete.zsh
@@ -1,22 +1,5 @@
 #!/usr/bin/env zsh
 
-_zsh_autosuggest_strategy__tabmit() {
-  local BUFFER_STR=${BUFFER}
-
-  if [[ "$BUFFER_STR" =~ ^git\ commit\ -m\ \"([^\"]*)$ ]]; then
-    local partial_msg="${match[1]}"
-    local suggestion
-
-    suggestion=$(tabmit "$partial_msg" 2>/dev/null)
-
-    if [[ -n "$suggestion" ]]; then
-      suggestion="${suggestion#${partial_msg}}"
-      echo "$suggestion\""
-    fi
-  fi
-}
-export ZSH_AUTOSUGGEST_STRATEGY=(_tabmit "$ZSH_AUTOSUGGEST_STRATEGY[@]")
-
 _tabmit() {
   if [[ $BUFFER == git\ commit\ -m\ * ]]; then
     local raw_input="${BUFFER#git commit -m }"
@@ -38,7 +21,6 @@ _tabmit() {
     echo "completion: $completion" >> /tmp/gpt_commit_msg.log
 
     if [[ -n "$completion" ]]; then
-      # local new_msg="${msg_prefix}${completion}"
       local new_msg="${completion}"
       local new_cmd="git commit -m \"${new_msg}\""
       echo "new_cmd: $new_cmd" >> /tmp/gpt_commit_msg.log
@@ -47,6 +29,8 @@ _tabmit() {
       BUFFER="$new_cmd"
       CURSOR=${#BUFFER}
       zle reset-prompt
+      # remove autosuggest if present
+      zle autosuggest-clear
     else
       zle -M "Error generating commit message."
     fi
